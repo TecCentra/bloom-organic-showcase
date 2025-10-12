2d7eec3064cfc900b39ce5db39da4d14fb523f64
896ma <solarcrash3@gmail.com>
2025-10-12 22:57:38 +0300

    redirect-featured-blog

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Contact from "./pages/Contact";
 import FeaturedBlogs from "./pages/FeaturedBlogs";
+import BlogDetail from "./pages/BlogDetail";
 import NotFound from "./pages/NotFound";
 import ProductDetail from "./pages/productid";
 import CheckoutPage from "./pages/checkoutPage";
@@ -22,6 +23,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/blogs" element={<FeaturedBlogs />} />
+          <Route path="/blog/:id" element={<BlogDetail />} />
           <Route path="/product/:id" element={<ProductDetail />} />
           <Route path="/cart" element={<CartPage />} />
           <Route path="/checkout" element={<CheckoutPage />} />
