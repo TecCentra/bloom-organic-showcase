1fa2e404f198ec44a3ff4c8c4a0fd14d60b3c0c2
896ma <solarcrash3@gmail.com>
2025-11-28 00:39:38 +0300

    payment-success

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import FAQs from "./pages/FAQs";
 import NotFound from "./pages/NotFound";
 import ProductDetail from "./pages/productid";
 import CheckoutPage from "./pages/checkoutPage";
+import PaymentSuccess from "./pages/PaymentSuccess";
 import CartPage from "./pages/cartPage";
 import RegisterForm from "./pages/signup";
 import AdminPanel from "./pages/admin";
@@ -27,6 +28,7 @@ import CategoriesPage from "./pages/CategoriesPage";
 import CategoryProducts from "./pages/CategoryProducts";
 import ForgotPassword from "./pages/ForgotPassword";
 import UserProfile from "./pages/UserProfile";
+
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -43,29 +45,30 @@ const App = () => (
                   <ScrollToTop />
                   <FloatingWhatsApp />
                   <Routes>
-                <Route path="/" element={<Index />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/blogs" element={<FeaturedBlogs />} />
-                <Route path="/blog/:id" element={<BlogDetail />} />
-                <Route path="/faqs" element={<FAQs />} />
-                <Route path="/shop" element={<AllProducts />} />
-                <Route path="/products" element={<CategoriesPage />} />
-                <Route path="/products/:slug" element={<CategoryProducts />} />
-                <Route path="/forgot-password" element={<ForgotPassword />} />
-                <Route path="/product/:id" element={<ProductDetail />} />
-                <Route path="/cart" element={<CartPage />} />
-                <Route path="/checkout" element={<CheckoutPage />} />
-                <Route path="/signup" element={<RegisterForm />} />-
-                <Route path="/profile" element={<UserProfile />} />
-                <Route path="/admin/*" element={
-                  <AdminAuthProvider>
-                    <AdminPanel />
-                  </AdminAuthProvider>
-                } />
-                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-                <Route path="*" element={<NotFound />} />
-                
-                </Routes>
+                    <Route path="/" element={<Index />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/blogs" element={<FeaturedBlogs />} />
+                    <Route path="/blog/:id" element={<BlogDetail />} />
+                    <Route path="/faqs" element={<FAQs />} />
+                    <Route path="/shop" element={<AllProducts />} />
+                    <Route path="/products" element={<CategoriesPage />} />
+                    <Route path="/products/:slug" element={<CategoryProducts />} />
+                    <Route path="/forgot-password" element={<ForgotPassword />} />
+                    <Route path="/product/:id" element={<ProductDetail />} />
+                    <Route path="/cart" element={<CartPage />} />
+                    <Route path="/checkout" element={<CheckoutPage />} />
+                    <Route path="/payment-success" element={<PaymentSuccess />} />
+                    <Route path="/signup" element={<RegisterForm />} />
+                    <Route path="/profile" element={<UserProfile />} />
+                    <Route path="/admin/*" element={
+                      <AdminAuthProvider>
+                        <AdminPanel />
+                      </AdminAuthProvider>
+                    } />
+                    {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                    <Route path="*" element={<NotFound />} />
+
+                  </Routes>
                 </CartProvider>
               </UserAuthProvider>
             </BrowserRouter>
